28931205b70e5508fdf55b6ba4634b29ee936807
elisa <elisaopalka@gmail.com>
2020-11-16 15:38:59 -0500

    commit tree dec1eff99b7fca2e122564c32d73f919851a11cc

--- prime.rb
+++ prime.rb
@@ -1,1 +1,27 @@
 # Add  code here!
+# def prime?()
+
+# end
+# def prime?(num)
+#     flag = true || false
+#     for x in (2..num -1)
+#         if num % 2 == 0
+#             flag = false
+#         end
+#     end
+#     return flag
+# end
+ 
+def prime?(num)
+    if num < 2
+        return false
+    elsif
+        (2..num - 1).each do |n|
+        if num % n == 0
+            return false
+        end
+    end
+
+    return true
+end
+end
